42a95f1c66a19c63036d20525714cd836504d9a3
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-12-09 16:03:26 +0100

    add files and folder for footer + fix responsive in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Footer from './components/footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Projet 3 : CJF Tennis</h1>
+      <Footer />
     </div>
   );
 }
