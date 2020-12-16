b224e5e35f7bc6c0d923d0f5247fca0e86dbb707
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-12-16 11:01:30 +0100

    add link back to front for icons

--- src/components/icons-info/IconsInfo.jsx
+++ src/components/icons-info/IconsInfo.jsx
@@ -1,23 +1,38 @@
 import './IconsInfo.css';
+import axios from 'axios';
 import { AiOutlineIdcard } from 'react-icons/ai';
 import { FiClock } from 'react-icons/fi';
 import { CgTennis } from 'react-icons/cg';
+import { useEffect, useState } from 'react';
+// import { BACK_URL } from '../../http';
  
 export default function IconsInfo() {
+    const [iconInfo, setIconInfo] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:8000/api/home/icons")
+        .then(res => {
+            setIconInfo(res.data);
+            setIsLoading(false);
+        })
+    })
+
     return(
         <section className="icons-info-container">
             <div className="icons-info-content">
                 <div className="icon-info">
                     <FiClock className="icon" />
-                    <p className="info">Depuis 1963</p>
+                    <p className="info">{isLoading? "Is loading" : iconInfo[0].body}</p>
                 </div>
                 <div className="icon-info">
                     <AiOutlineIdcard className="icon" />
-                    <p className="info">+ de 350 licenciés</p>
+                    <p className="info">{isLoading? "Is loading" : iconInfo[1].body}</p>
                 </div>
                 <div className="icon-info">
                     <CgTennis className="icon" />
-                    <p className="info">Des cours accessibles à tous</p>
+                    <p className="info">{isLoading? "Is loading" : iconInfo[2].body}</p>
                 </div>
             </div>
         </section>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import BannerVideo from '../../components/banner-video/BannerVideo';
 import AccessClub from '../../components/access-club/AccessClub';
+import IconsInfo from '../../components/icons-info/IconsInfo';
 
 function Home() {
   return (
     <div className="Home">
         <BannerVideo source="http://localhost:3000/upload/tennis_intro.mp4" title="CJF Tennis" subtitle="Fleury-les-Aubrais" />
+        <IconsInfo />
         <AccessClub />     
     </div>
   );
