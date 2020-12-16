9f4cc6b0961fd1f4335eede7e38c6a17d3b8543b
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-12-16 10:11:51 +0100

    link back to front for the video

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <Switch></Switch>
       </Router>
 
-      <BannerVideo source="http://localhost:3000/upload/tennis_intro.mp4" title="CJF Tennis" subtitle="Ici c'est Fleury" />
+      <BannerVideo />
       <IconsInfo />
       <ArticleCard article={article_example} side="left" />
       <AccessClub />
