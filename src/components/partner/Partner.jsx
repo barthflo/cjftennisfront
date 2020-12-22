a69a26f30a7ed999deabaa758c822d9822b99d5c
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-12-22 13:42:33 +0100

    add partner section + link back to front + update readme

---
+++ src/components/partner/Partner.jsx
@@ -0,0 +1,92 @@
+import './Partner.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import SectionTitle from '../../components/section-title/SectionTitle';
+import axios from 'axios';
+import { BACK_URL, DOMAIN_URL } from '../../http';
+import { useEffect, useState } from 'react';
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+      slidesToSlide: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+};
+
+export default function Partner(){
+    const [partners, setPartners] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(`${BACK_URL}/home/partners`)
+        .then(res => setPartners(res.data))
+    }, []);
+
+    return(
+        <section className="partner-club">
+            <SectionTitle title="Nos partenaires" color="white" />
+            <Carousel
+                additionalTransfrom={0}
+                arrows
+                autoPlay
+                autoPlaySpeed={4000}
+                centerMode={false}
+                className="partner-carousel"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite
+                itemClass="slider-partner"
+                keyBoardControl
+                minimumTouchDrag={80}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                  desktop: {
+                    breakpoint: {
+                      max: 3000,
+                      min: 1024
+                    },
+                    items: 3,
+                  },
+                  tablet: {
+                    breakpoint: {
+                      max: 1024,
+                      min: 665
+                    },
+                    items: 2,
+                    partialVisibilityGutter: 30
+                  },
+                  mobile: {
+                    breakpoint: {
+                      max: 664,
+                      min: 0
+                    },
+                    items: 1,
+                    partialVisibilityGutter: 30
+                  }
+                }}
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={1}
+            >
+                {partners.map((partner) => (
+                    <a href={partner.link_partner} target="blank" key={partner.id}>
+                        <img src={`${DOMAIN_URL}/upload/${partner.image_url}`} alt={partner.name} className="image-partner"/>
+                    </a>
+                ))}
+            </Carousel>
+        </section>
+    );
+}
