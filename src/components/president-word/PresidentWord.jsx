bb15067c7b52d95b10f0c5b221912c7bd58b1c85
BilalCHERFA <cherfabilal@gmail.com>
2021-01-04 11:52:01 +0100

    ok

--- src/components/president-word/PresidentWord.jsx
+++ src/components/president-word/PresidentWord.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {FETCH} from './../../Fetch';
+import {BACK_URL, DOMAIN_URL} from './../../http';
 import "./PresidentWord.css";
 import SectionTitle from "../section-title/SectionTitle";
 
@@ -8,12 +8,12 @@ import SectionTitle from "../section-title/SectionTitle";
 
 export default function PresidentWord() {
    
-   const [presidetWord, setPresidentWord] = useState([]);
+   const [presidentWord, setPresidentWord] = useState([]);
     useEffect(() => {
         const fetchPresidentWord = () => {
            axios
-           .get(`${FETCH}/club/president-word`)
-           .then(res => setPresidentWord(res.data))
+           .get(`${BACK_URL}/club/president-word`)
+           .then(res => setPresidentWord(res.data[0]))
         }
         fetchPresidentWord()   
      }, []);
@@ -23,18 +23,18 @@ export default function PresidentWord() {
     return (
         <div  className="presidetWord-container">
             <SectionTitle className="section-title" title = "Le mot de la présidente" />       
-            {presidetWord.map((president) => (
+           
 
-                <div key = {president.id} className="president">
-                        <img className="president_image" src= "http://localhost:3000/upload/photoPresident.jpg" alt={president.lastname}/>
+                <div className="president">
+                        <img className="president_image" src={`${DOMAIN_URL}/upload/${presidentWord.picture_url}`} alt={presidentWord.lastname}/>
                         
                         <div className="president-word"> 
-                            <h2 className="president-name">{president.firstname}{" "}{president.lastname} <br/> Présidente de la CJF Tennis</h2>
+                            <h2 className="president-name">{presidentWord.firstname}{" "}{presidentWord.lastname} <br/> Présidente de la CJF Tennis</h2>
                     
-                            <p className="description"> "{president.description}." </p>
+                            <p className="description"> "{presidentWord.description}." </p>
                         </div>
                </div>
-            ))}        
+                   
         </div>
     );
 }
