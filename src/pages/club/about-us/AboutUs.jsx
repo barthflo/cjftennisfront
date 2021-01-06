75b4f6a2390663746dc942d8bf9633c5741146f2
BilalCHERFA <cherfabilal@gmail.com>
2021-01-06 10:29:28 +0100

    formatage AboutUs

--- src/pages/club/about-us/AboutUs.jsx
+++ src/pages/club/about-us/AboutUs.jsx
@@ -1,14 +1,11 @@
 import BannerPhoto from "../../../components/banner-photo/BannerPhoto";
 import PresidentWord from '../../../components/president-word/PresidentWord'
 
-
-
 function AboutUs() {
   return (
     <div className="AboutUs">
       <BannerPhoto image="https://wallpaperaccess.com/full/780473.jpg" title="A Propos de Nous" subtitle="Cjf Tennis"/>
       <PresidentWord />
-
     </div>
   );
 }
