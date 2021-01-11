5c24f9ad18c7c5154b64f65b2af0c6570485a9b4
AbiEmmanuel <abiakreemmanuel@gmail.com>
2021-01-11 10:56:55 +0100

    add pictures in upload

--- src/components/banner-photo/BannerPhoto.jsx
+++ src/components/banner-photo/BannerPhoto.jsx
@@ -2,7 +2,7 @@ import './BannerPhoto.css';
 
 export default function BannerPhoto({ image, title, subtitle }){
     return(
-        <section className="banner-photo">
+        <section className="banner-photo position-flex">
             <div className="image-bg" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${image})`}}></div>
             <div className="content">
                 <h1 className="main-title">{title}</h1>
--- src/pages/enseignement/team-teaching/TeamTeaching.jsx
+++ src/pages/enseignement/team-teaching/TeamTeaching.jsx
@@ -1,13 +1,19 @@
+import { TiThListOutline } from "react-icons/ti";
 import BannerPhoto from "../../../components/banner-photo/BannerPhoto";
 import Teaching_Team from "../../../components/teaching/teaching_team/Teaching_Team";
-
+import './Teaching_Team.css';
+import SectionTitle from "../../../components/section-title/SectionTitle"
 
 function TeamTeaching() {
   return (
-    <div className="TeamTeaching">
+    <main className="TeamTeaching position-absolue">
       <BannerPhoto image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/135617429/original/ed8df4e402c3cec184266e41a32e2418d1d14e15/be-your-personal-tennis-coach.jpg" title="Equipe Enseignante" subtitle="Cjf Tennis"/>
+      <section className="teaching_team">
+      <SectionTitle title='Les Formateurs'/>
       <Teaching_Team />
-    </div>
+      </section>
+
+    </main>
   );
 }
 
