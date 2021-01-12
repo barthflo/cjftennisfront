0e9094cd11c64e8e7d00058b3032c3b37dd81ff7
florentdesmarets <florent.desmarets@gmail.com>
2021-01-12 12:04:23 +0100

    first commit calendar

---
+++ src/components/calendar-comp/SportCalendar.jsx
@@ -0,0 +1,53 @@
+import React, {Fragment, useState, useEffect, createRef} from 'react';
+import {BACK_URL, DOMAIN_URL} from '../../http';
+import Axios from 'axios';
+import SectionTitle from '../section-title/SectionTitle';
+
+
+
+const SportCalendar = (props) => {
+
+    const [datas, setDatas] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    useEffect(() => {
+        const fetchData = () => {
+            Axios.get(`${BACK_URL}/competitions/sport/calendar`)
+                 .then(res => {
+                     if(datas.length < res.data.length){
+                        setDatas(res.data.filter(data => data.archived === 0));
+                        setIsLoading(false);
+                     }
+                 });
+        }
+        fetchData();
+    }, [datas]);
+    console.log(datas)
+    return (
+       <section>
+           {!isLoading && 
+        <Fragment> 
+            { datas.length === 0 ? 
+                <p className="calendar-unvalable">Calendrier indisponible</p>
+            : 
+            <Fragment>
+                <SectionTitle title={datas[0].title} color="white"/>
+                <div className="container-image-calendar">
+                        {datas.map((data, index) => {
+                            return(
+                                <div key={index} className="calendar-image">
+                                    <img className="calendarComp" src={`${DOMAIN_URL}/upload/${data.picture_url}`}/>
+                                </div> 
+                            )
+                        }
+                                
+                        )}
+                </div>
+          </Fragment>
+        }
+        </Fragment> }
+       </section>
+    )
+}
+
+export default SportCalendar;
--- src/pages/competition/calendar/Calendar.jsx
+++ src/pages/competition/calendar/Calendar.jsx
@@ -1,11 +1,12 @@
 import BannerPhoto from "../../../components/banner-photo/BannerPhoto";
-
+import SportCalendar from"../../../components/calendar-comp/SportCalendar";
 
 function Calendar() {
   return (
-    <div className="Calendar">
+    <main className="Calendar">
       <BannerPhoto image="https://u-paris.fr/wp-content/uploads/2019/02/Sans-titre-68.jpg" title="Calendrier" subtitle="Cjf Tennis"/>
-    </div>
+      <SportCalendar/>
+    </main>
   );
 }
 
