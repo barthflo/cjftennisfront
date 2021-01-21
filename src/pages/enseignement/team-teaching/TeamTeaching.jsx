297e6fbd4c1a4ff0cb3a266442d18a4db9004d85
AbiEmmanuel <abiakreemmanuel@gmail.com>
2021-01-21 14:56:11 +0100

    add picture and change title padding

--- src/pages/enseignement/team-teaching/TeamTeaching.jsx
+++ src/pages/enseignement/team-teaching/TeamTeaching.jsx
@@ -7,10 +7,10 @@ import SectionTitle from "../../../components/section-title/SectionTitle";
 function TeamTeaching() {
   return (
     <main className="TeamTeaching">
-      <BannerPhoto image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/135617429/original/ed8df4e402c3cec184266e41a32e2418d1d14e15/be-your-personal-tennis-coach.jpg" title="Equipe Enseignante" subtitle="Cjf Tennis"/>
+      <BannerPhoto image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/135617429/original/ed8df4e402c3cec184266e41a32e2418d1d14e15/be-your-personal-tennis-coach.jpg" title="Equipe Enseignante" subtitle="Cjf Tennis" />
       <section className="teaching_team">
-      <SectionTitle title='Les Formateurs'/>
-      <Teaching_Team />
+        <SectionTitle title='Les Formateurs'/>
+        <Teaching_Team />
       </section>
 
     </main>
