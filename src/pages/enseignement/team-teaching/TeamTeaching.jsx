516be57ed712aa28199628f518e449efc0c6f5a3
AbiEmmanuel <abiakreemmanuel@gmail.com>
2021-01-25 11:27:54 +0100

    add assistants team

--- src/pages/enseignement/team-teaching/TeamTeaching.jsx
+++ src/pages/enseignement/team-teaching/TeamTeaching.jsx
@@ -1,6 +1,7 @@
 import { TiThListOutline } from "react-icons/ti";
 import BannerPhoto from "../../../components/banner-photo/BannerPhoto";
-import Teaching_Team from "../../../components/teaching/teaching_team/Teaching_Team"
+import Teaching_Team from "../../../components/teaching/teaching_team/Teaching_Team";
+import Assistant_Teaching_Team from '../../../components/teaching/teaching_team/Assistant_Teaching_Team'
 import './Teaching_Team.css';
 import SectionTitle from "../../../components/section-title/SectionTitle";
 
@@ -10,7 +11,9 @@ function TeamTeaching() {
       <BannerPhoto image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/135617429/original/ed8df4e402c3cec184266e41a32e2418d1d14e15/be-your-personal-tennis-coach.jpg" title="Equipe Enseignante" subtitle="Cjf Tennis" />
       <section className="teaching_team">
         <SectionTitle title='Les Formateurs'/>
-        <Teaching_Team />
+        <Teaching_Team className="formateurs" />
+        <SectionTitle title='Les Assistants Formateurs' className="assistants"/>
+        <Assistant_Teaching_Team />
       </section>
 
     </main>
