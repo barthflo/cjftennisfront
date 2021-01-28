484eed93d11b173778850e06995e5283937ed569
theo.atlani@gmail.com <theo.atlani@gmail.com>
2021-01-28 16:07:29 +0100

    fix bugs

--- src/pages/enseignement/team-teaching/TeamTeaching.jsx
+++ src/pages/enseignement/team-teaching/TeamTeaching.jsx
@@ -1,7 +1,6 @@
 import BannerPhoto from "../../../components/banner-photo/BannerPhoto";
 import TeachingTeam from "../../../components/teaching/teaching_team/Teaching_Team";
-import Teaching_Team from "../../../components/teaching/teaching_team/Teaching_Team";
-import Assistant_Teaching_Team from '../../../components/teaching/teaching_team/Assistant_Teaching_Team';
+import AssistantTeachingTeam from '../../../components/teaching/teaching_team/Assistant_Teaching_Team';
 import './Teaching_Team.css';
 import SectionTitle from "../../../components/section-title/SectionTitle";
 import { useEffect } from 'react';
@@ -16,9 +15,9 @@ function TeamTeaching() {
       <BannerPhoto image="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/135617429/original/ed8df4e402c3cec184266e41a32e2418d1d14e15/be-your-personal-tennis-coach.jpg" title="Equipe Enseignante" subtitle="Cjf Tennis" />
       <section className="teaching_team">
         <SectionTitle title='Les Formateurs' />
-        <Teaching_Team className="formateurs" />
+        <TeachingTeam className="formateurs" />
         <SectionTitle title='Les Assistants Formateurs' className="assistants_formateurs" />
-        <Assistant_Teaching_Team />
+        <AssistantTeachingTeam />
       </section>
     </main>
   );
